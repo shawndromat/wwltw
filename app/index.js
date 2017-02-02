618b1139f708ba423afe8e27d4fd70a82f924dd0
Shawna Mattison <pair+smattison@pivotal.io>
2017-02-02 15:27:14 -0500

    Add fancy dropdown

--- app/index.js
+++ app/index.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 class Item {
     constructor(id) {
         this.$elem = $(this.generateItem(id))
@@ -23,22 +21,44 @@ class Item {
         }
     }
 
+    emailBodyContent() {
+        let tags = this.findTags(this.$elem.find('input:checked')).join(', ');
+        return (this.$elem.find('textarea').val() + '\n' + 'Tags: ' + tags + '\n');
+    }
+
+    populateTags() {
+        var tagList = ['ruby', 'js'];
+        let tagHtml = '';
+        tagList.map((tag) => {
+           tagHtml += `<option value="${tag}">${tag}</option>`
+        });
+
+        return tagHtml;
+    }
+
     generateItem(id) {
         return `
         
                 <div data-item-id=${id}>
                     <div class="form-group">
-                        <label for=${"item-" + id}>Item ${id}</label>
+                        <h3 for=${"item-" + id}>Item ${id}</h3>
                         <textarea name=${"item-" + id} id=${"item-" + id} cols="30" rows="5" class="form-control"></textarea>
                     </div>
                     <div class="checkbox">
-                        <h3>Tags</h3>
-                        <label for=${"item-" + id + "java"}><input type="checkbox" id=${"item-" + id + "java"} value="java" />Java</label>
-                        <label for=${"item-" + id + "ruby"}><input type="checkbox" id=${"item-" + id + "ruby"} value="ruby" />Ruby</label>
+                        <h4>Tags </h4>
+                        <select name="tags" multiple="" class="ui fluid dropdown">
+                            <option value="">Tags</option>
+                            ${this.populateTags()}
+                        </select>
                     </div>
                 </div>
             `
+        // <label for=${"item-" + id + "java"}><input type="checkbox" id=${"item-" + id + "java"} value="java" />Java</label>
+        // <label for=${"item-" + id + "ruby"}><input type="checkbox" id=${"item-" + id + "ruby"} value="ruby" />Ruby</label>
+        // <label for=${"item-" + id + "discussion"}><input type="checkbox" id=${"item-" + id + "discussion"} value="discussion" />Discussion</label>
     }
+
+
 }
 
 class Form {
@@ -61,6 +81,7 @@ class Form {
     onSubmit(e) {
         e.preventDefault();
         let teamName = this.$teamName.val();
+        let emailContent = '';
 
         let formattedEmail = `
         <div class="well">
@@ -75,8 +96,13 @@ class Form {
         this.items.map((item) => {
             if(item.formattedContent()){
                 formattedEmail += item.formattedContent();
+                emailContent += item.emailBodyContent();
             }
         });
+
+        var emailRecipent = encodeURIComponent('pivotalk+wwltw@gmail.com')
+        var emailSubject = encodeURIComponent(`WWLTW ${teamName}`);
+        var emailBody = encodeURIComponent(emailContent);
 
         formattedEmail +=
             `
@@ -84,6 +110,7 @@ class Form {
                     <p>-${teamName} team</p>
                 </div>
             </div>
+            <a class="btn btn-primary" target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=${emailRecipent}&su=${emailSubject}&body=${emailBody}">Send</a>
             </div>
             `
 
@@ -101,5 +128,6 @@ class Form {
 
 $(() => {
     new Form('form');
+     $('.ui.fluid.dropdown').dropdown();
 });
 
